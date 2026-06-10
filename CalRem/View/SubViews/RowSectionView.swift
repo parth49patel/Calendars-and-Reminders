41d1649fb9ad8a3fdc11ecb9314053378c201a0e
Parth Patel <4parth9@gmail.com>
2026-06-09 23:21:46 -0400

    Removed suiteName from UserDefaults

--- CalRem/View/SubViews/RowSectionView.swift
+++ CalRem/View/SubViews/RowSectionView.swift
@@ -1,18 +1,18 @@
 //
-//  DaysOfWeekView.swift
+//  RowSectionView.swift
 //  CalRem
 //
-//  Created by Parth Patel on 2026-05-25.
+//  Created by Parth Patel on 2026-06-04.
 //
 
 import SwiftUI
 
-struct DaysOfWeekView: View {
+struct RowSectionView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DaysOfWeekView()
+    RowSectionView()
 }
